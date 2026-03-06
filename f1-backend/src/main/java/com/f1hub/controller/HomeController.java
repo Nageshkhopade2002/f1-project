e3930336efb4d1186a566009cd87cff0dc4f6c2c
Nageshkhopade2002 <nageshkhopade47@gmail.com>
2026-03-06 12:28:08 +0530

    Add HomeController with a home endpoint

---
+++ f1-backend/src/main/java/com/f1hub/controller/HomeController.java
@@ -0,0 +1,13 @@
+package com.f1hub.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+    @GetMapping("/")
+    public String home() {
+        return "F1Hub Backend Running 🚀";
+    }
+}
